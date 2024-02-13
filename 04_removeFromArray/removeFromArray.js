e4bf682cc281125c038e5dfef2d0c1747e64069c
Luke <luke.g.schwade@gmail.com>
2024-02-13 17:19:43 -0500

    All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeThese) {
+    return array.filter(checkRemoval);
 
+    function checkRemoval(currentElement) {
+        for (const element of removeThese) {
+            if (element === currentElement) {
+                return false;
+            } 
+        }
+        return true;
+    }
+    //return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

fe6acb50ca654ca94a9073109e1e173b3f5295dd
Luke <luke.g.schwade@gmail.com>
2024-02-13 16:39:24 -0500

    reverseString passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let splitWord = [];
+    for (let index = 0; index < word.length; index++) {
+        splitWord.unshift(word.at(index));
+        
+    }
+    return splitWord.join('');
 };
 
 // Do not edit below this line

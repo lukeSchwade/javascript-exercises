a7933f2ea84ba46e829251f71f75a8d1d1d1a1e6
Luke <luke.g.schwade@gmail.com>
2024-02-13 16:07:50 -0500

    repeat string passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(inputStr, numberTimes) {
+    let result = '';
+    if (numberTimes < 0) {
+        result = 'ERROR';
+    } else {
+        for (let x = 0; x < numberTimes; x++) {
+            result = result.concat(inputStr);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line

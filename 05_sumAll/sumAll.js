013fdb6eab35d8d0520dfacbbc23ab45e9bca070
Luke <luke.g.schwade@gmail.com>
2024-02-13 17:52:23 -0500

    passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    if (firstNum < 0 || lastNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+        sum = "ERROR";
+    } else {
+        if (firstNum > lastNum) [firstNum, lastNum] = [lastNum, firstNum];
+        
+        for (let index = firstNum; index <= lastNum; index++) {
+            sum += index;
+            
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
